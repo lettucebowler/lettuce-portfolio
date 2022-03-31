2e5ad8fb18e15eeed96d6d5007b01dbd2c58ebea
Grant Montgomery <grantm@rusondia.net>
2022-03-31 12:04:34 -0500

    aria-hidden added to project figures and slight rewording

--- src/lib/components/SudokuProject.svelte
+++ src/lib/components/SudokuProject.svelte
@@ -8,11 +8,9 @@
 	<svelte:fragment slot="description">
 		<a href="https://sudoku.rusondia.net" target="_blank"><h2>Sveltedoku</h2></a>
 		<p>
-			Simple sudoku game. I made this game to give myself a reason to learn <a
-				href="https://kit.svelte.dev"
-				target="_blank">svelte-kit</a
-			>. Playable demo available at <a href="https://sudoku.rusondia.net" target="_blank">here</a>.
-			Code available for browsing on
+			Made using <a href="https://kit.svelte.dev" target="_blank">svelte-kit</a>. Simple sudoku
+			game. <a href="https://sudoku.rusondia.net" target="_blank">Playable here</a>. Browse the code
+			on
 			<a href="https://github.com/lettucebowler/sveltedoku" target="_blank">Github</a>.
 		</p>
 	</svelte:fragment>
