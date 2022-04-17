d41fe256f3fda39df31bb06c9f32335ccd1bbfdc
Grant Montgomery <grantm@rusondia.net>
2022-04-17 15:58:51 -0500

    a11y fix

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <figure>
-	<a {href} target="_blank"><span class={`icon-${icon} ${color}`} alt={caption} /></a>
+	<a {href} title={caption} target="_blank"><span class={`icon-${icon} ${color}`} alt={caption} /></a>
 	<figcaption>{caption}</figcaption>
 </figure>
 
