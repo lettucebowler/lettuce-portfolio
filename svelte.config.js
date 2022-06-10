066c12cd1d70860f0049a2de7d1964766be73838
Grant Montgomery <lettucebowler@gmail.com>
2022-06-10 18:50:32 -0500

    move to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({ out: 'build' }),
+		adapter: adapter({ out: 'build', edge: true }),
 		prerender: {
 			default: true
 		}
