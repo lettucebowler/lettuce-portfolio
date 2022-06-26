9d5fabbf6c4a1854b688eae70001d5932d1d7431
Grant Montgomery <lettucebowler@gmail.com>
2022-06-26 17:34:19 -0500

    remove adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({ out: 'build', edge: true }),
+		adapter: adapter({ out: 'build' }),
 		prerender: {
 			default: true
 		}
