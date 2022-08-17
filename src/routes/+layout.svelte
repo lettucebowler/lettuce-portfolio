e6ba600aef032b77d19923a5bfb6207ef93897aa
Grant Montgomery <lettucebowler@gmail.com>
2022-08-17 01:15:43 -0500

    bleh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '$lib/assets/icomoon.css';
 	import '$lib/assets/app.css';
+	import smallFavicon from '$lib/assets/favicon-16x16.png';
+	import bigFavicon from '$lib/assets/favicon-32x32.png';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import safariPinnedTabIcon from '$lib/assets/safari-pinned-tab.svg';
 
 	const links = [
 		{
@@ -15,6 +19,13 @@
 		}
 	];
 </script>
+
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={bigFavicon} />
+	<link rel="icon" type="image/png" sizes="16x16" href={smallFavicon} />
+	<link rel="mask-icon" href={safariPinnedTabIcon} color="#a3be8c" />
+</svelte:head>
 
 <PageContentContainer>
 	<Navbar {links} />
