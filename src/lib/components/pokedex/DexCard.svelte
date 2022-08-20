18eb92e65e77a53175490cc8ec782baef1e07db8
Grant Montgomery <lettucebowler@gmail.com>
2022-08-20 11:37:20 -0500

    bleh

--- src/lib/components/pokedex/DexCard.svelte
+++ src/lib/components/pokedex/DexCard.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="rounded-lg aspect-square grid p-1 border-[#fae078] bg-snow-300">
-	<img src={pikachu} alt="pikachu" class="m-auto aspect-square" />
+	<img src={pikachu} alt="pikachu" class="m-auto aspect-square h-full" />
 	<div class="bg-[#f8d030] grid place-items-center text-[black] rounded font-medium p-1">
 		electric
 	</div>
