9769aac6158a127be131fa6b7bd5bc0fae33ddff
Grant Montgomery <lettucebowler@gmail.com>
2022-08-20 11:42:55 -0500

    bleh

--- src/lib/components/pokedex/DexCard.svelte
+++ src/lib/components/pokedex/DexCard.svelte
@@ -2,8 +2,12 @@
 	import pikachu from '$lib/assets/25.webp';
 </script>
 
-<div class="rounded-lg aspect-square grid p-1 border-[#fae078] bg-snow-300">
-	<img src={pikachu} alt="pikachu" class="m-auto aspect-square h-full" />
+<div class="rounded-lg aspect-square grid p-1 border-[#fae078] bg-snow-300 w-full">
+	<img
+		src="https://cdn.dex.rusondia.net/home/25.webp"
+		alt="pikachu"
+		class="m-auto aspect-square w-80 m-auto"
+	/>
 	<div class="bg-[#f8d030] grid place-items-center text-[black] rounded font-medium p-1">
 		electric
 	</div>
