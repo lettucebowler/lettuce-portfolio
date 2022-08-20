054e0993970c0b1a18a35da5780d2793c0910585
Grant Montgomery <lettucebowler@gmail.com>
2022-08-20 11:21:44 -0500

    finish tailwind conversion

--- src/lib/components/sudoku/SudokuBlock.svelte
+++ src/lib/components/sudoku/SudokuBlock.svelte
@@ -1,21 +1,12 @@
 <script>
-	const order = 3;
 	export let board = false;
 	import classnames from 'classnames';
 </script>
 
-<div class={classnames('three bg-polar-300 grid gap-[1px]', { board: board })}>
+<div
+	class={classnames('grid-cols-3 grid-rows-3 bg-polar-300 grid gap-[1px]', {
+		'gap-[3px] aspect-square': board
+	})}
+>
 	<slot />
 </div>
-
-<style>
-	.three {
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-template-rows: 1fr 1fr 1fr;
-	}
-
-	.board {
-		grid-gap: 3px;
-		aspect-ratio: 1;
-	}
-</style>
--- src/lib/components/sudoku/SudokuCell.svelte
+++ src/lib/components/sudoku/SudokuCell.svelte
@@ -1,80 +1,19 @@
 <script lang="ts">
+	import classnames from 'classnames';
 	export let number = '';
-	export let row: number;
-	export let col: number;
 	export let selected = false;
 	export let peerCell = false;
 	export let peerDigit = false;
-	export let order = 3;
-
-	const top = row === 0;
-	const bottom = row === order * order - 1;
-	const left = col === 0;
-	const right = col === order * order - 1;
-
-	const getClass = (
-		selected: boolean,
-		peerCell: boolean,
-		peerDigit: boolean,
-		top: boolean,
-		right: boolean,
-		bottom: boolean,
-		left: boolean,
-		number: string
-	) => {
-		const classes = [];
-		number === '0' && classes.push('hidden');
-		selected && classes.push('selected');
-		peerCell && classes.push('peerCell');
-		peerDigit && classes.push('peerDigit');
-		top && right && classes.push('topRight');
-		top && left && classes.push('topLeft');
-		bottom && right && classes.push('bottomRight');
-		bottom && left && classes.push('bottomLeft');
-		return classes.join(' ');
-	};
-
-	$: classString = getClass(selected, peerCell, peerDigit, top, right, bottom, left, number);
 </script>
 
-<div class={classString}>
+<div
+	class={classnames('grid place-items-center text-polar-100 select-none font-medium', {
+		'text-transparent': number === '0',
+		'bg-aurora-300': selected,
+		'bg-snow-100': !selected && !peerCell && !peerDigit,
+		'bg-frost-200': peerCell,
+		'bg-frost-300': peerDigit
+	})}
+>
 	{number}
 </div>
-
-<style>
-	div {
-		display: grid;
-		align-items: center;
-		background-color: var(--background-color);
-		color: var(--text-color);
-		place-items: center;
-		-webkit-user-select: none; /* Safari */
-		-moz-user-select: none; /* Firefox */
-		-ms-user-select: none; /* IE10+/Edge */
-		user-select: none; /* Standard */
-	}
-
-	.hidden {
-		color: transparent;
-	}
-
-	.selected {
-		background-color: var(--nord-13);
-	}
-
-	.peerCell {
-		background-color: var(--nord-8);
-	}
-
-	.peerDigit {
-		background-color: var(--nord-9);
-	}
-
-	.initial {
-		color: var(--nord-2);
-	}
-
-	.inputselect {
-		caret-color: transparent;
-	}
-</style>
